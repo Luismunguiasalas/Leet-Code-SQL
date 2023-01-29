6fd28157b802f617b6b1cd3ed6b6cddf93a1196f
Luis Munguia Salas <36023613+Luismunguiasalas@users.noreply.github.com>
2023-01-28 21:49:57 -0800

    Update easy.sql
    solved # 1623. All valid triplets that can represent a country

--- easy.sql
+++ easy.sql
@@ -99,3 +99,8 @@ having balance > 10000
 select date_format(day '%W, %M %e, %Y') as 'day'
 from Days
 
+-- 1623. All Valid Triplets That Can Represent a Country
+select a.student_name as 'member_A', b.student_name as 'member_B', c.student_name as 'member_C'
+from SchoolA as a, SchoolB as b, SchoolC as c
+where a.student_name != b.student_name and a.student_name!= c.student_name and b.student_name != c.student_name
+and a.student_id != b.student_id and a.student_id != c.student_id and b.student_id != c.student_id
